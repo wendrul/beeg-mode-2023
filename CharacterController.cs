6eed682895d1ea027eb2aa5398ac2b603f314824
wendrul <dethomassin.etienne@gmail.com>
2023-11-30 13:02:24 +0900

    Fix the renaming of the project
    Make a new project and start from zero but without dirty renamings that
    seemed to have broken the last version.

--- CharacterController.cs
+++ CharacterController.cs
@@ -1,13 +1,18 @@
 using Godot;
 using System;
 
-public partial class characterTest : KinematicBody2D
+public partial class CharacterController : KinematicBody2D
 {
-    public const float Speed = 300.0f;
-    public const float JumpVelocity = -400.0f;
-    public float gravity = 1200f;
+    [Export] public float Speed = 300.0f;
+    [Export] public float JumpVelocity = -400.0f;
+    [Export] public float gravity = 1200f;
     
     private Vector2 Velocity;
+
+    public override void _Ready()
+    {
+        GD.Print("Ready!!");
+    }
 
     public override void _PhysicsProcess(float delta)
     {
@@ -18,14 +23,12 @@ public partial class characterTest : KinematicBody2D
             velocity.y += gravity * (float)delta;
 
         // Handle Jump.
-        if (Input.IsActionJustPressed("ui_select") && IsOnFloor()){
+        if (Input.IsActionJustPressed("jump") && IsOnFloor()){
             velocity.y = JumpVelocity;
         }
 
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        // Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        float horiz = (Input.IsActionPressed("ui_right") ? 1f:0f) - (Input.IsActionPressed("ui_left") ? 1f:0f);
+        float horiz = (Input.IsActionPressed("move_right") ? 1f:0f) 
+                        - (Input.IsActionPressed("move_left") ? 1f:0f);
         Vector2 direction = new Vector2(horiz, 0);
         if (direction != Vector2.Zero)
         {
